6112b9fa7c6253363889c28db906163d2dc242fe
catfishjim <jjnospam68@gmail.com>
2025-02-28 08:43:19 -0500

    rr

--- src/main/java/com/practicetestautomation/base/BrowserDriverFactory.java
+++ src/main/java/com/practicetestautomation/base/BrowserDriverFactory.java
@@ -25,14 +25,14 @@ public class BrowserDriverFactory {
 
 		switch (browser) {
 		case "chrome":
-			// Make sure to upgrade chromedriver to work with your browser version: https://chromedriver.chromium.org/downloads
+			
 		//	System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 			System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
 			driver.set(new ChromeDriver());
 			break;
 
 		case "edge":
-			// Make sure to upgrade geckodriver to work with your browser version: https://github.com/mozilla/geckodriver/releases
+			
 		//	System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
 		//	System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
 		//	System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "/dev/null");
@@ -40,7 +40,7 @@ public class BrowserDriverFactory {
 			break;
 
 		default:
-			log.debug("Do not know how to start: " + browser + ", starting chrome.");
+			log.debug("default.");
 			//System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 			System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
 			driver.set(new ChromeDriver());
